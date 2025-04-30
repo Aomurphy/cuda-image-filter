189ab2470bd7e3ddc3fdc2f0315a589daeb40a1a
April Murphy <71937654+Aomurphy@users.noreply.github.com>
2025-04-30 17:55:58 -0400

    Update filters.h

--- src/filters.h
+++ src/filters.h
@@ -1,1 +1,9 @@
-initial
+#ifndef FILTERS_H
+#define FILTERS_H
+
+#include <opencv2/opencv.hpp>
+
+// Applies a 3x3 box blur using CUDA
+void apply_blur(const cv::Mat& input, cv::Mat& output);
+
+#endif  // FILTERS_H
